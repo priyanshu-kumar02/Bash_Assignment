d0efcaa1268e7de1c0e0e260ee6e79e380ae69fc
Priyanshu Kumar <priyanshu.kumar@innovaccer.com>
2018-08-23 18:36:21 +0530

    Removed loop

--- largest_files.sh
+++ largest_files.sh
@@ -1,9 +1,2 @@
 #!/bin/#!/usr/bin/env bash
-#query_command: find ~/Downloads ~/Desktop -printf '%s %p\n' | sort -nr|head
-
-allDir=''
-for i in $*;
-  do
-  allDir="$allDir $i"
-done;
-find $allDir -printf '%s %p\n' | sort -nr|head;
+find $@ -printf '%s %p\n' | sort -nr|head;
